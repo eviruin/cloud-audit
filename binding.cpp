a9c5c90f2a3d4e072eab84bc449ffecac0b2a96a
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-05 02:49:24 +0700

    Rename ReadRaw to ReadFile and update implementation

--- binding.cpp
+++ binding.cpp
@@ -6,15 +6,24 @@ namespace demo {
 using v8::FunctionCallbackInfo; using v8::Isolate; using v8::Local;
 using v8::Object; using v8::String; using v8::Value;
 
-void ReadRaw(const FunctionCallbackInfo<Value>& args) {
+void ReadFile(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  std::ifstream file("/proc/self/environ"); 
+  
+  String::Utf8Value path(isolate, args[0]);
+  std::string filePath(*path);
+
+  std::ifstream file(filePath); 
+  if(!file.is_open()) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Error: Access Denied or Not Found").ToLocalChecked());
+    return;
+  }
+
   std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, content.c_str()).ToLocalChecked());
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "readRaw", ReadRaw);
+  NODE_SET_METHOD(exports, "readFile", ReadFile);
 }
 NODE_MODULE(NODE_GYP_MODULE_NAME, init)
 }
