a92e814421ad6679e9606222753ca868bd576f8e
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-05 11:10:45 +0700

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -1,39 +1,23 @@
-#include <node.h>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iomanip>
+#include <sys/stat.h>
+#include <unistd.h>
 
-namespace demo {
-using v8::FunctionCallbackInfo; using v8::Isolate; using v8::Local;
-using v8::Object; using v8::String; using v8::Value;
-
-void ReadHex(const FunctionCallbackInfo<Value>& args) {
+void CheckStat(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   String::Utf8Value path(isolate, args[0]);
   std::string filePath(*path);
-  
-  std::ifstream file(filePath, std::ios::binary);
-  if(!file.is_open()) {
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Error: Access Denied").ToLocalChecked());
+
+  struct stat st;
+  if (stat(filePath.c_str(), &st) != 0) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Not Found").ToLocalChecked());
     return;
   }
 
-  std::vector<unsigned char> buffer(512);
-  file.read((char*)buffer.data(), buffer.size());
-  std::streamsize bytesRead = file.gcount();
+  std::string type = "Unknown";
+  if (S_ISSOCK(st.st_mode)) type = "SOCKET (JACKPOT!)";
+  else if (S_ISDIR(st.st_mode)) type = "Directory";
+  else if (S_ISREG(st.st_mode)) type = "Regular File";
 
   std::stringstream ss;
-  ss << std::hex << std::setfill('0');
-  for (int i = 0; i < bytesRead; ++i)
-    ss << std::setw(2) << (int)buffer[i];
-
+  ss << type << " | Perms: " << std::oct << (st.st_mode & 0777);
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, ss.str().c_str()).ToLocalChecked());
-}
-
-void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "readHex", ReadHex);
-}
-NODE_MODULE(NODE_GYP_MODULE_NAME, init)
 }
