12b0a532a8464e12ce2a271ef4c16027498c56aa
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-05 10:59:50 +0700

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -1,29 +1,35 @@
 #include <node.h>
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
+#include <memory>
 
 namespace demo {
 using v8::FunctionCallbackInfo; using v8::Isolate; using v8::Local;
 using v8::Object; using v8::String; using v8::Value;
 
-void ReadFile(const FunctionCallbackInfo<Value>& args) {
+void Exec(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
+  String::Utf8Value cmd(isolate, args[0]);
+  std::string command(*cmd);
   
-  String::Utf8Value path(isolate, args[0]);
-  std::string filePath(*path);
-
-  std::ifstream file(filePath); 
-  if(!file.is_open()) {
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Error: Access Denied or Not Found").ToLocalChecked());
+  std::array<char, 256> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen((command + " 2>&1").c_str(), "r"), pclose);
+  
+  if (!pipe) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Error: popen failed").ToLocalChecked());
     return;
   }
-
-  std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, content.c_str()).ToLocalChecked());
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, result.c_str()).ToLocalChecked());
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "readFile", ReadFile);
+  NODE_SET_METHOD(exports, "exec", Exec);
 }
 NODE_MODULE(NODE_GYP_MODULE_NAME, init)
 }
