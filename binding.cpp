4576276ee0ad558845c7c9517bf50b0b0eb19a99
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-09 01:08:03 +0700

    Add files via upload

--- binding.cpp
+++ binding.cpp
@@ -1,52 +1,33 @@
 #include <napi.h>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <sys/stat.h>
-#include <dirent.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
-using namespace Napi;
+Napi::Value ProbePath(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    std::string path = info[0].As<Napi::String>();
 
-Value Execute(const CallbackInfo& info) {
-    Env env = info.Env();
-    std::string command = info[0].As<String>().Utf8Value();
-    char buffer[128];
-    std::string result = "";
-    FILE* pipe = popen((command + " 2>&1").c_str(), "r");
-    if (!pipe) return String::New(env, "Error");
-    while (fgets(buffer, sizeof(buffer), pipe) != NULL) { result += buffer; }
-    pclose(pipe);
-    return String::New(env, result);
-}
-
-Value GetInode(const CallbackInfo& info) {
-    Env env = info.Env();
-    if (info.Length() < 1) return Number::New(env, -1);
-    std::string path = info[0].As<String>().Utf8Value();
     struct stat st;
+    Napi::Object res = Napi::Object::New(env);
+
+    res.Set("exists", Napi::Boolean::New(env, access(path.c_str(), F_OK) == 0));
+    res.Set("readable", Napi::Boolean::New(env, access(path.c_str(), R_OK) == 0));
+
     if (stat(path.c_str(), &st) == 0) {
-        return Number::New(env, (double)st.st_ino);
+        res.Set("mode", Napi::Number::New(env, st.st_mode));
+        res.Set("uid", Napi::Number::New(env, st.st_uid));
+    } else {
+        res.Set("error", Napi::String::New(env, strerror(errno)));
     }
-    return Number::New(env, -1);
+
+    return res;
 }
 
-Value RawRead(const CallbackInfo& info) {
-    Env env = info.Env();
-    std::string path = info[0].As<String>().Utf8Value();
-    FILE* f = fopen(path.c_str(), "r");
-    if (!f) return String::New(env, "Access Denied");
-    char buf[1024];
-    std::string content = "";
-    while (fgets(buf, sizeof(buf), f)) content += buf;
-    fclose(f);
-    return String::New(env, content);
-}
-
-Object Init(Env env, Object exports) {
-    exports.Set("execute", Function::New(env, Execute));
-    exports.Set("getInode", Function::New(env, GetInode));
-    exports.Set("rawRead", Function::New(env, RawRead));
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "probe"), Napi::Function::New(env, ProbePath));
     return exports;
 }
 
