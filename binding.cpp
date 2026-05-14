3cc36dcabda2c21e1578458e85d5747079a6d293
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-05-15 01:53:11 +0700

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -3,6 +3,7 @@
 #include <linux/io_uring.h>
 #include <sys/syscall.h>
 #include <sys/mman.h>
+#include <sys/uio.h> // <-- Tambahin ini biar iovec nggak error
 #include <unistd.h>
 #include <string.h>
 #include <vector>
@@ -14,33 +15,41 @@ void TestMemoryMapping(const FunctionCallbackInfo<Value>& args) {
     struct io_uring_params p;
     memset(&p, 0, sizeof(p));
     
+    // Setup ring
     int ring_fd = syscall(__NR_io_uring_setup, 32, &p);
     std::string log = "";
 
     if (ring_fd < 0) {
-        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "[-] Setup failed").ToLocalChecked());
+        log = "[-] io_uring_setup failed. Errno: " + std::to_string(errno) + "\n";
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, log.c_str()).ToLocalChecked());
         return;
     }
 
-    // Alokasi buffer besar untuk mencoba spraying
-    size_t buf_size = 1024 * 1024; // 1MB
+    // Alokasi buffer 1MB
+    size_t buf_size = 1024 * 1024;
     void* buffer = mmap(NULL, buf_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    memset(buffer, 0x41, buf_size); // Fill with 'A'
-
-    struct iovec iov;
-    iov.iov_base = buffer;
-    iov.iov_len = buf_size;
-
-    // Daftarkan buffer ke kernel
-    int reg_res = syscall(__NR_io_uring_register, ring_fd, IORING_REGISTER_BUFFERS, &iov, 1);
     
-    if (reg_res < 0) {
-        log += "[-] Buffer registration failed. Errno: " + std::to_string(errno) + " (Normal if restricted)\n";
+    if (buffer == MAP_FAILED) {
+        log = "[-] mmap failed\n";
     } else {
-        log += "[+] SUCCESS: Kernel accepted 1MB registered buffer. KASLR bypass attempt possible.\n";
+        memset(buffer, 0x41, buf_size);
+
+        struct iovec iov;
+        iov.iov_base = buffer;
+        iov.iov_len = buf_size;
+
+        // Register buffer ke kernel
+        int reg_res = syscall(__NR_io_uring_register, ring_fd, IORING_REGISTER_BUFFERS, &iov, 1);
+        
+        if (reg_res < 0) {
+            log += "[-] IORING_REGISTER_BUFFERS failed. Errno: " + std::to_string(errno) + "\n";
+            if (errno == 1) log += "[!] EPERM: Kernel memblokir registrasi buffer (Expected in some hardened jails).\n";
+        } else {
+            log += "[+] SUCCESS: 1MB Buffer registered in kernel space! This is huge.\n";
+        }
+        munmap(buffer, buf_size);
     }
 
-    munmap(buffer, buf_size);
     close(ring_fd);
     args.GetReturnValue().Set(String::NewFromUtf8(isolate, log.c_str()).ToLocalChecked());
 }
