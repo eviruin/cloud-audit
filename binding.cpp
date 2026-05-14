b450386e7785941d8dfa04b3e53bff5c40bda926
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-05-15 01:51:18 +0700

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -2,44 +2,51 @@
 #include <v8.h>
 #include <linux/io_uring.h>
 #include <sys/syscall.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <string.h>
+#include <vector>
 
 using namespace v8;
 
-// Hardcoded features kalau header ketinggalan zaman
-#ifndef IORING_FEAT_SQPOLL
-#define IORING_FEAT_SQPOLL (1U << 1)
-#endif
-
-void TriggerRing(const FunctionCallbackInfo<Value>& args) {
+void TestMemoryMapping(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     struct io_uring_params p;
     memset(&p, 0, sizeof(p));
-
-    std::string log = "[*] Kernel: 6.12.81-cloudflare-firecracker\n";
-
-    // Mencoba setup io_uring
-    int ring_fd = syscall(__NR_io_uring_setup, 32, &p);
     
+    int ring_fd = syscall(__NR_io_uring_setup, 32, &p);
+    std::string log = "";
+
     if (ring_fd < 0) {
-        log += "[-] io_uring_setup BLOCKED or NOT SUPPORTED. Errno: " + std::to_string(errno) + "\n";
-    } else {
-        log += "[+] SUCCESS: io_uring_setup is ALLOWED! FD: " + std::to_string(ring_fd) + "\n";
-        
-        if (p.features & IORING_FEAT_SQPOLL) {
-            log += "[!] SQPOLL feature is AVAILABLE. This is a major attack vector!\n";
-        } else {
-            log += "[*] SQPOLL not active in this params set.\n";
-        }
-        close(ring_fd);
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "[-] Setup failed").ToLocalChecked());
+        return;
     }
 
+    // Alokasi buffer besar untuk mencoba spraying
+    size_t buf_size = 1024 * 1024; // 1MB
+    void* buffer = mmap(NULL, buf_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    memset(buffer, 0x41, buf_size); // Fill with 'A'
+
+    struct iovec iov;
+    iov.iov_base = buffer;
+    iov.iov_len = buf_size;
+
+    // Daftarkan buffer ke kernel
+    int reg_res = syscall(__NR_io_uring_register, ring_fd, IORING_REGISTER_BUFFERS, &iov, 1);
+    
+    if (reg_res < 0) {
+        log += "[-] Buffer registration failed. Errno: " + std::to_string(errno) + " (Normal if restricted)\n";
+    } else {
+        log += "[+] SUCCESS: Kernel accepted 1MB registered buffer. KASLR bypass attempt possible.\n";
+    }
+
+    munmap(buffer, buf_size);
+    close(ring_fd);
     args.GetReturnValue().Set(String::NewFromUtf8(isolate, log.c_str()).ToLocalChecked());
 }
 
 void Init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "triggerRing", TriggerRing);
+    NODE_SET_METHOD(exports, "testMemory", TestMemoryMapping);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
